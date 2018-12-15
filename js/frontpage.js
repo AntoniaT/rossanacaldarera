f697d86b261fe514cfbb2c97cbd4843c431f5b1c
Antonia <gintoni@gmx.at>
2018-12-15 19:55:14 +0100

    set up

--- js/frontpage.js
+++ js/frontpage.js
@@ -1,23 +1,22 @@
-const template1 = document.querySelector("#frontpageTemplate");
-const parent1 = document.querySelector("#whoIamContainer");
+const template = document.querySelector("#frontpageTemplate");
+const parent = document.querySelector("#whoIamContainer");
+//
+//const params = new URLSearchParams(window.location.search);
+//const catID = params.get("cat");
 
-const params = new URLSearchParams(window.location.search);
-const catID = params.get("cat");
 
-
-//function loadPosts() {
-//    fetch("http://anckdesign.com/kea/rossana/wp-json/wp/v2/Front_page?"+"categories="+catID+"&_embed").then(e => e.json()).then(showPost);
-//}
 function loadPosts() {
-    fetch("http://anckdesign.com/kea/rossana/wp-json/wp/v2/Front_page?categories="+"2"+"&_embed").then(e => e.json()).then(showPost);
+    fetch("http://anckdesign.com/kea/rossana/wp-json/wp/v2/front_page?categories="+"2"+"&_embed").then(e => e.json()).then(showPost);
+    console.log();
 }
 function showPost(post){
-    const copy = template1.cloneNode(true).content;
+    const clone= template.cloneNode(true);
 
-    copy.querySelector("p").innerHTML=post.content.rendered;
-    copy.querySelector("img").src = post._embedded["wp:featuredmedia"][0].media_details.sizes.medium.source_url;
+    clone.querySelector("#sectionContent").textContent=post.content.rendered;
+    clone.querySelector("#sectionImg").src = post._embedded["wp:featuredmedia"][0].media_details.sizes.medium.source_url;
     console.log(post);
-}
-    parent1.appendChild(template1);
 
-showPost();
+    parent.appendChild(clone);
+
+}
+
